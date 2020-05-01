a194303668b2b3e169e1453920d7df5a9cc6bcbb
vseredovych <victor.seredovich@gmail.com>
2020-05-01 22:52:26 +0300

    fixed nn

--- Practice/Text Classification/NeuralNetwork.py
+++ Practice/Text Classification/NeuralNetwork.py
@@ -23,160 +23,124 @@ def plot_data(A, b, test = False):
     plt.scatter(A[negative_indices, 0], A[negative_indices, 1], marker='+', c= 'blue' if test else 'red')
 
 class NeuralNet:
-    """
-    NN for binary classification
-    Attributes:
-    ...
-    """
-    # hidden_layer_sizes=(100, 50,)
-    def __init__(self, hidden_layer_sizes, normalize = True, learning_rate = 0.01, num_iter = 30000, batch_size=64, optimization='', momentum=0.9):
-        self.learning_rate = learning_rate
-        self.num_iter = num_iter
-        self.normalize = normalize
+    def __init__(self, hidden_layer_sizes=(10, ), normalize=True, learning_rate=0.01, max_iter=30000, batch_size=32, momentum=0, epsilon=1e-10):
         self.hidden_layer_sizes = hidden_layer_sizes
-        self.optimization = optimization
+        self.num_layers = len(hidden_layer_sizes) + 1
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
+        self.normalize = normalize
         self.momentum = momentum
         self.batch_size = batch_size
+        self.epsilon = epsilon
         
-    def __normalize(self, X, mean = None, std = None):
-        """
-        Зверніть увагу, що нормалізація вхідних даних є дуже важливою для швидкодії нейронних мереж.
-        """
-        n = X.shape[0]
-    
-        if mean is None:   
-            mean = np.zeros([n, 1])
-        if std is None:
-            std  = np.ones([n, 1])
-        
-        for i in range(n):
-            if (np.std(X[:, i]) != 0):
-                if mean is None:
+    def __normalize(self, X, mean=None, std=None):
+        if mean is None or std is None:
+            mean = np.zeros([X.shape[0], 1])
+            std  = np.ones([X.shape[0], 1])
+
+            for i in range(X.shape[0]):
+                if (np.std(X[:, i]) != 0):
                     mean[i] = np.mean(X[:, i])
-                if std is None:
                     std[i] = np.std(X[:, i])
-        
+                
         X_new = (X - mean) / std
         return X_new, mean, std
     
 
     def __sigmoid(self, Z):
-        """
-        В наступних практичних потрібно буде додати підтримку й інших активаційних функцій - це один з гіперпараметрів. 
-        Їх можна вибирати для всіх шарів одночасно або мати різні активаційні функції на кожному з них.
-        """
         return 1 / (1 + np.exp(-Z))
     
     def __sigmoid_derivative(self, Z):
         z = self.__sigmoid(Z)
         return np.multiply(z, (1 - z))
     
-    def __softmax(self, Z):
-        exp_z = np.exp(Z)
-        return exp_z / exp_z.sum(axis=0, keepdims=True)
+    def __stable_softmax(self, z):
+        exps = np.exp(z - np.max(z))
+        return exps / exps.sum(axis=0, keepdims=True)
     
     def __cross_entropy(self, A, Y):
-        return - np.sum(Y * np.log(A), axis=1)
+        return -np.mean(Y.T * np.log(A.T))
     
-    def __initialize_parameters(self, n_x, n_y):
+    def __initialize_parameters(self, n_x, n_y, hidden_layers):
+        W = []
+        b = []
+        self.VdW = []
+        self.Vdb = []
+        
         self.parameters = {}
-        n = len(n_x)
-        
-        for i in range(1, len(n_x)):
-            W = np.random.randn(n_x[i], n_x[i - 1]) * 0.01
-            b = np.zeros((n_x[i], 1))
-            self.parameters.update({f"W{i}": W, f"b{i}": b})
-
-        W = np.random.randn(n_y, n_x[n-1]) * 0.01
-        b = np.zeros((n_y, 1))
-        
-        self.parameters.update({f"W{n}":W, f"b{n}":b})
-                    
-        for i in range(1, len(n_x) + 1):
-            self.parameters.update({f"VdW{i}": 0, f"Vdb{i}": 0})
+        layers = [n_x] + list(hidden_layers) + [n_y]
+        for i in range(1, self.num_layers + 1):
+            W.append(np.random.randn(layers[i], layers[i - 1]) * 0.01)
+            b.append(np.zeros((layers[i], 1)))
             
-    def __forward_propagation(self, X):
-        num_layers = len(self.hidden_layer_sizes)
-        cache = self.parameters.copy()
+            self.VdW.append(np.zeros((layers[i], layers[i - 1])))
+            self.Vdb.append(np.zeros((layers[i], 1)))
         
-        A = X
-        for i in range(1, num_layers + 2):
-            if i == num_layers + 1:
-                W = self.parameters[f"W{i}"]
-                b = self.parameters[f"b{i}"]
-                Z = np.dot(W, A) + b
-                A = self.__softmax(Z)
-            else:            
-                W = self.parameters[f"W{i}"]
-                b = self.parameters[f"b{i}"]
-                Z = np.dot(W, A) + b
-                A = self.__sigmoid(Z)
-            cache.update({f"Z{i}": Z})
-            cache.update({f"A{i}": A})
-        return A, cache
+        self.parameters.update({"W": W, "b": b})
+                    
+    def __forward_propagation(self, X):
+        W = self.parameters["W"]
+        b = self.parameters["b"]
+        
+        A = []
+        Z = []
+        
+        for i in range(0, self.num_layers - 1):
+            if i == 0:
+                Z.append(np.dot(W[i], X) + b[i])
+            else:
+                Z.append(np.dot(W[i], A[i-1]) + b[i])
+            A.append(self.__sigmoid(Z[i]))
+        
+        Z.append(np.dot(W[-1], A[-1]) + b[-1])
+        A.append(self.__stable_softmax(Z[-1]))
+        
+        cache = (W, b, Z, A)
 
-    def compute_cost(self, A, Y):
-        J = -np.mean(Y.T * np.log(A.T))
-        return J
+        return A[-1], cache
     
     def __backward_propagation(self, X, Y, cache):
         m = X.shape[1]
         n = X.shape[0]
-        num_layers = len(self.hidden_layer_sizes)
+        (W, b, Z, A) = cache
         
-        grads = {}
+        dW = [None] * self.num_layers
+        dA = [None] * self.num_layers
+        dZ = [None] * self.num_layers
+        db = [None] * self.num_layers
         
-        for i in range(num_layers+1, 0, -1):
-            W = cache[f"W{i}"]
-            b = cache[f"b{i}"]
-            Z = cache[f"Z{i}"]
-            if i == num_layers + 1:
-                A = cache[f"A{i}"]
-                A_next = cache[f"A{i - 1}"]
-                
-                dZ = A - Y
-                dW = 1. / m * np.dot(dZ, A_next.T)
-                db = 1. / m * np.sum(dZ, axis = 1, keepdims = True)
-            elif (i == 1):
-                W_prev = cache[f"W{i + 1}"]
-                A = cache[f"A{i}"]
-                
-                dA = np.dot(W_prev.T, dZ)
-                dZ = np.multiply(dA, self.__sigmoid_derivative(A))
-                dW = 1. / m * np.dot(dZ, X.T)
-                db = 1. / m * np.sum(dZ, axis = 1, keepdims = True)
+        dZ[-1] = A[-1] - Y
+        dW[-1] = (1. / m) * np.dot(dZ[-1], A[-2].T)
+        db[-1] = (1. / m) * np.sum(dZ[-1], axis = 1, keepdims = True)
+        dA[-1] = np.dot(W[-1].T, dZ[-1])
+        
+        for i in range(self.num_layers-1, 0, -1):
+            dZ[i-1] = np.multiply(dA[i], self.__sigmoid_derivative(Z[i-1]))
+            if i-1 == 0:
+                dW[i-1] = 1. / m * np.dot(dZ[i-1], X.T)
             else:
-                W_prev = cache[f"W{i + 1}"]
-                A = cache[f"A{i}"]
-                A_next = cache[f"A{i - 1}"]
-
-                dA = np.dot(W_prev.T, dZ)
-                dZ = np.multiply(dA, self.__sigmoid_derivative(A))
-                dW = 1. / m * np.dot(dZ, A_next.T)
-                db = 1. / m * np.sum(dZ, axis = 1, keepdims = True)
-            grads.update({f"dZ{i}":dZ, f"dW{i}":dW, f"db{i}":db})
+                dW[i-1] = 1. / m * np.dot(dZ[i-1], A[i-2].T)
+            db[i-1] = 1. / m * np.sum(dZ[i-1], axis = 1, keepdims = True) 
+            dA[i-1] = np.dot(W[i-1].T, dZ[i-1])
+        
+        grads = {"dZ" : dZ, "dW" : dW, "db" : db}
         return grads
     
     def __update_parameters(self, grads, iteration):
-        num_layers = len(self.hidden_layer_sizes)
-        
-        for i in range(1, num_layers + 2):
-            W = self.parameters[f"W{i}"]
-            b = self.parameters[f"b{i}"]
+        for i in range(self.num_layers):
+            W = self.parameters[f"W"]
+            b = self.parameters[f"b"]
             
-            dW = grads[f"dW{i}"]
-            db = grads[f"db{i}"]
-            if self.optimization == 'momentum':
-#                 print(1 - self.momentum ** iteration)
-                self.parameters[f"VdW{i}"] = (self.momentum * self.parameters[f"VdW{i}"] + (1 - self.momentum) * dW / (1 - self.momentum ** (iteration + 1)))
-                self.parameters[f"Vdb{i}"] = (self.momentum * self.parameters[f"Vdb{i}"] + (1 - self.momentum) * db / (1 - self.momentum ** (iteration + 1)))
+            dW = grads[f"dW"]
+            db = grads[f"db"]
+            
+            self.VdW[i] = (self.momentum * self.VdW[i] + (1 - self.momentum) * dW[i] / (1 - self.momentum ** (iteration + 1)))
+            self.Vdb[i] = (self.momentum * self.Vdb[i] + (1 - self.momentum) * db[i] / (1 - self.momentum ** (iteration + 1)))
 
-                self.parameters[f"W{i}"] = W - self.learning_rate * self.parameters[f"VdW{i}"]
-                self.parameters[f"b{i}"] = b - self.learning_rate * self.parameters[f"Vdb{i}"]
-
-            else:
-                self.parameters[f"W{i}"] = W - self.learning_rate * dW
-                self.parameters[f"b{i}"] = b - self.learning_rate * db
+            W[i] = W[i] - self.learning_rate * self.VdW[i]
+            b[i] = b[i] - self.learning_rate * self.Vdb[i]
+            
+            self.parameters = {"W" : W, "b" : b}
     
     def __create_mini_batches(self, X, Y, batch_size):
         m = X.shape[1]
@@ -189,62 +153,56 @@ class NeuralNet:
 
         mini_batches = []
         n_minibatches = m // batch_size
-        i = 0
-
-        for i in range(n_minibatches + 1): 
+        
+        for i in range(n_minibatches): 
             mini_batch = data[:, i * batch_size:(i + 1)*batch_size] 
             X_mini = mini_batch[:x_columns, :] 
             Y_mini = mini_batch[x_columns:, :]
             mini_batches.append((X_mini, Y_mini))
-            
-            if data.shape[0] % batch_size != 0:
-                mini_batch = data[:, i * batch_size:m] 
-                X_mini = mini_batch[:x_columns, :]
-                Y_mini = mini_batch[x_columns:, :]
-                mini_batches.append((X_mini, Y_mini)) 
+        if m % batch_size != 0:
+            mini_batch = data[:, (i+1) * batch_size:m] 
+            X_mini = mini_batch[:x_columns, :]
+            Y_mini = mini_batch[x_columns:, :]
+            mini_batches.append((X_mini, Y_mini)) 
         return mini_batches
     
-    def fit(self, X_vert, Y_vert, epsilon=1e-08, print_cost = True):
-        
-        X, Y = X_vert.T, np.expand_dims(Y_vert, axis=1).T
-        
+    def fit(self, X_vert, Y_vert, print_cost=True, print_by=1000):
+        X, Y = X_vert.T, Y_vert.T
         if self.normalize:
             X, self.__mean, self.__std = self.__normalize(X)
-        
         costs = []
         
         m = X.shape[1]
-        n_x = (X.shape[0],) + self.hidden_layer_sizes
+        n_x = X.shape[0]
         n_y = Y.shape[0]
         
-        self.__initialize_parameters(n_x, n_y)
+        self.__initialize_parameters(n_x, n_y, self.hidden_layer_sizes)
         
-        for i in range(self.num_iter):
+        for i in range(self.max_iter):
             mini_batches = self.__create_mini_batches(X, Y, self.batch_size) 
-            for mini_batch in mini_batches:
-                X_mini, Y_mini = mini_batch 
+            for (X_mini, Y_mini) in mini_batches:
                 A, cache = self.__forward_propagation(X_mini)
 
-                cost = self.compute_cost(A, Y_mini)
+                cost = self.__cross_entropy(A, Y_mini)
 
                 grads = self.__backward_propagation(X_mini, Y_mini, cache)
 
                 self.__update_parameters(grads, i)
 
-                costs.append(cost)
+            costs.append(cost)
 
-                if print_cost and i % 1000 == 0:
-                    print("{}-th iteration: {}".format(i, cost))
-                    if i > 1:
-                        print(f"Delta: {costs[-2] - costs[-1]}")
+            if print_cost and i % print_by == 0:
+                print("{}-th iteration: {}".format(i, cost))
+                if i > 1:
+                    print(f"Delta: {costs[-2] - costs[-1]}")
 
-                if i > 1 and abs(costs[-2] - costs[-1]) < epsilon:
-                    break
+            if i > 1 and abs(costs[-2] - costs[-1]) < self.epsilon:
+                break
                 
         if print_cost:
             plt.plot(costs)
             plt.ylabel("Cost")
-            plt.xlabel("Iteration, *1000")
+            plt.xlabel(f"Iteration, *{print_by}")
             plt.show()
 
     def predict_proba(self, X_vert):
